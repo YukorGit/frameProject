d8a607f15845a624c571941d4d1ee624f5ed14ab
yuriy <yuriyseraz@gmail.com>
2025-07-23 22:47:28 +0300

    update index.php

--- project/www/index.php
+++ project/www/index.php
@@ -28,7 +28,13 @@ try {
 
     $controller = new $controllerName();
     $controller->$actionName(...$matches);
+} catch (\MyProject\Exceptions\DbException $e) {
+    $view = new \MyProject\View\View(__DIR__ . '/../templates/errors');
+    $view->renderHtml('500.php', ['error' => $e->getMessage()], 500);
 } catch (\MyProject\Exceptions\NotFoundException $e) {
     $view = new \MyProject\View\View(__DIR__ . '/../templates/errors');
     $view->renderHtml('404.php', ['error' => $e->getMessage()], 404);
+} catch (\MyProject\Exceptions\UnauthorizedException $e) {
+    $view = new \MyProject\View\View(__DIR__ . '/../templates/errors');
+    $view->renderHtml('401.php', ['error' => $e->getMessage()], 401);
 }
